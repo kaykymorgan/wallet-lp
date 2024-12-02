9480103fdaa1d2ff47d101757b99cb885a7fb753
JhonnyFrade <fradeww@gmail.com>
2024-12-02 18:04:40 -0300

    style: ajuste de formatação

--- src/Components/Partners.tsx
+++ src/Components/Partners.tsx
@@ -1,7 +1,7 @@
 
 export default function Partners() {
   return (
-    <div className="max-w-screen-xl mx-auto  flex justify-center items-center">
+    <div className="max-w-screen-xl mx-auto flex justify-center items-center">
         <img src="src/assets/partners.png" alt="" className="w-2/3 py-20" />
     </div>
   )
